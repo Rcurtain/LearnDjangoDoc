7e668e8205605ba5ea0afce76461f8656252a2c4
NJ <r_curtain@163.com>
2018-11-03 11:02:21 +0800

    django 8 add django bootstrap-admin,is really cool

--- test1/settings.py
+++ test1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap_admin', # always before django.contrib.admin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
