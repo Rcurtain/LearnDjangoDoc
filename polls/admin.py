3b647ee29f7594f8a2fc67747398483706f22acf
NJ <r_curtain@163.com>
2018-11-03 10:19:02 +0800

    django 7 admin custom

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['question_text', 'pub_date',]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        ('Question information', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
